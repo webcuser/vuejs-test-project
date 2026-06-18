21528fee1f6301979c5a4141e20440eb4c39d1b0
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:56:01 +0200

    feat: Set up Vue.js project structure

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Home View</h1>
+    <ExampleComponent />
+  </div>
+</template>
+
+<script>
+import ExampleComponent from '../components/ExampleComponent.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    ExampleComponent,
+  },
+};
+</script>
+
+<style scoped>
+/* Add styles for Home view */
+</style>
